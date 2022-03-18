53fbc53393d553fdf47170e7b3d27e881c5bd28d
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-03-18 17:29:44 +0300

    Kot_A_6 - task done

--- src/Kot_A_6.kt
+++ src/Kot_A_6.kt
@@ -1,42 +1,24 @@
-import java.io.*
+import java.io.File
 
 /*
 В каждой строке стихотворения подсчитать частоту повторяемости каждого слова и вывести эти слова в порядке
 возрастания частоты повторяемости.
  */
-/*
-??????? ошибка NPE
- */
+
 fun main() {
-    var s: MutableList<String>
-    var bf: BufferedReader? = null
-    var pw: PrintWriter? = null
-    try {
-        bf = BufferedReader(FileReader(File("C:\\Users\\Admin\\Desktop\\", "Chapter_8_input.txt")))
-        pw = PrintWriter(BufferedWriter(FileWriter(File("C:\\Users\\Admin\\Desktop\\", "Chapter_8_output.txt"))))
-        var tmp: String
-        while (bf.readLine().also { tmp = it } != null) {
-            s = tmp.split(" ".toRegex()).toMutableList()
-            var count = 0
-            for (i in 0 until s.size - 1) {
-                for (j in i + 1 until s.size) {
-                    if (s[i] == s[j] && i != j) {
-                        count += 1
-                        println("${s[j]} повторяется $count раз")
-                    }
-                }
+    val s: MutableList<String> = mutableListOf()
+    val str: String
+
+    val file = File("C:\\Users\\Admin\\Desktop\\", "Chapter_8_input.txt")
+    str = file.readText()
+    s.addAll((str.split(" ".toRegex())))
+    var count = 0
+    for (i in 0 until s.size - 1) {
+        for (j in i + 1 until s.size) {
+            if (s[i] == s[j] && i != j) {
+                count += 1
+                println("${s[j]} повторяется $count раз")
             }
-        }
-    } catch (e: IOException) {
-        e.printStackTrace()
-    } finally {
-        if (bf != null) {
-            try {
-                bf.close()
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-            pw?.close()
         }
     }
 }
